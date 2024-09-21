932a792785a35928fb2e784514cad8e87849bd11
Chris Joakim <christopher.joakim@gmail.com>
2024-09-21 15:32:01 -0400

    pyproject.toml and build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,11 @@
 # https://packaging.python.org/en/latest/tutorials/packaging-projects/
 # Uses file pyproject.toml
 
+rm -rf dist/
+
 python3 -m build
+
+# Check the contents of the build file:
+#   cd dist/
+#   gunzip m26-0.3.0.tar.gz
+#   tar tvf m26-0.3.0.tar
